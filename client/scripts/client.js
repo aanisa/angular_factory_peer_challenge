07e6c30a9fb7bac41fa126f03d47e919eb04bc4e
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-04-06 16:50:55 -0500

    little styling and commentary

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,50 +1,45 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('InputController', ['$scope', 'MovieService' , function($scope, MovieService ){
-$scope.Moviesobj = MovieService.movie;
-$scope.storeMoviefunc = MovieService.storeMovie;
-
-
+myApp.controller('InputController', ['$scope', 'MovieService', function($scope, MovieService) {
+    $scope.Moviesobj = MovieService.movie;
+    $scope.storeMoviefunc = MovieService.storeMovie;
 }]);
 
-myApp.controller('OutputController', ['$scope', 'MovieService' , function($scope, MovieService){
-
-
-$scope.movieArray = MovieService.movieArray;
-
+myApp.controller('OutputController', ['$scope', 'MovieService', function($scope, MovieService) {
+    $scope.movieArray = MovieService.movieArray;
 }]);
 
-myApp.factory('MovieService', [function(){
+myApp.factory('MovieService', [function() {
+    var movieArray = [];
 
-  var movieArray = [];
+    var movieObj = {
+        title: '',
+        description: '',
+        director: '',
+        duration: ''
+    };
 
-  var movieObj = {
-    title: '',
-    description: '',
-    director: '',
-    duration: ''
-  };
+    //function will store the movie object inside of an array
+    function storeMovie(aMovie) {
+        var movieCopy = angular.copy(aMovie); //makes a copy of the object -  that way when editing the input field, it won't re-write the savedMovie on the DOM
+        movieArray.push(movieCopy);
+        console.log(movieArray);
+        clearInput(aMovie);
+    }
 
+    //clear input fields - call this function in storeMovie() so it clears the fiels when the button is clicked
+    function clearInput(movie) {
+        movie.title = ('');
+        movie.description = ('');
+        movie.director = ('');
+        movie.duration = ('');
+    }
 
-function storeMovie(aMovie) {
-  var movieCopy = angular.copy(aMovie);
-  movieArray.push(movieCopy);
-  console.log(movieArray);
-  clearInput(aMovie);
-}
-
-function clearInput(movie) {
-  movie.title = ('');
-  movie.description = ('');
-  movie.director = ('');
-  movie.duration = ('');
-}
-
-  return {
-    movie: movieObj,
-    movieArray: movieArray,
-    storeMovie: storeMovie
-
-  };
+    //public variables
+    return {
+        movie: movieObj,
+        movieArray: movieArray,
+        storeMovie: storeMovie
+    };
 
 }]);
